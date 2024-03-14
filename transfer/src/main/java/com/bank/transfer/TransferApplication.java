69b16228b264561672229fbfb85aafe9a5861031
Erik Marx <impostor175@gmail.com>
2024-03-14 17:09:22 +0000

    подключить общий рест хэндлер[bk-5]

--- transfer/src/main/java/com/bank/transfer/TransferApplication.java
+++ transfer/src/main/java/com/bank/transfer/TransferApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bank.common", "com.bank.transfer"} )
 public class TransferApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransferApplication.class, args);
