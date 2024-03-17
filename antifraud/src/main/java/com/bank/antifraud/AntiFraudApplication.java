ddaed01a2f61204012b393171600cb51f474a62f
Ilgiz Nigmatullin <skorpizzzz@gmail.com>
2024-03-17 14:22:58 +0000

    Подключение общего Рэст Хендлера [bk-2]

--- antifraud/src/main/java/com/bank/antifraud/AntiFraudApplication.java
+++ antifraud/src/main/java/com/bank/antifraud/AntiFraudApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bank.common.handler",
+        "com.bank.antifraud"})
 public class AntiFraudApplication {
     public static void main(String[] args) {
         SpringApplication.run(AntiFraudApplication.class, args);
