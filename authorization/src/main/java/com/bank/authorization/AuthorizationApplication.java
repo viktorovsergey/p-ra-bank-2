aecebc3a72bba3bbdc6b243d4d73e2b3d4fc84eb
Peter Mikhalevsky <petermikh09@gmail.com>
2024-03-19 14:00:38 +0000

    Подключение общего Рэст Хендлера [bk-3]

--- authorization/src/main/java/com/bank/authorization/AuthorizationApplication.java
+++ authorization/src/main/java/com/bank/authorization/AuthorizationApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bank.common", "com.bank.authorization"})
 public class AuthorizationApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationApplication.class, args);
